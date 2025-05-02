5290fb32786d929aa4232b984c74f762441ebb10
shivesh2334-ai <drshivesh@gmail.com>
2025-05-02 14:05:04 +0530

    Add files via upload

---
+++ AboutServlet.java
@@ -0,0 +1,17 @@
+package com.example.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/about")
+public class AboutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("content", "We are a team of professionals dedicated to providing the best services to our clients.");
+        request.getRequestDispatcher("about.jsp").forward(request, response);
+    }
+}
